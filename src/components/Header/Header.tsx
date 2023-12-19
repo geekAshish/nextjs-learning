bdb379b01b61d804f08a4118d7280fa7381e30ca
ashish <ashish@wishfy.club>
2023-12-19 10:35:03 +0530

    layout

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <>
+      <header>
+        <div className="bg-orange-300">
+          <h1>This is header</h1>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
